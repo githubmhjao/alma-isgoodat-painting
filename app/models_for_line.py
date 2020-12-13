86b0f8ac4a258db0fff1902eaaa7ae007ddbbf72
githubmhjao <ntumhjao@gmail.com>
2020-12-13 16:47:38 +0800

    Create models_for_line.py

---
+++ app/models_for_line.py
@@ -0,0 +1,19 @@
+from app import handler
+from app.custom_models import AlmaTalks
+
+from linebot.models import MessageEvent, TextMessage, ImageMessage, PostbackEvent
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    AlmaTalks.default_reply(event.source.user_id, event.message.text, event.reply_token)
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    AlmaTalks.phase_start(event)
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if not event.postback.data.startswith('second_tone='):
+        AlmaTalks.phase_intermediate(event)
+    else:
+        AlmaTalks.phase_finish(event)
