896f57cbc255233040a5ca9ae7970311b3fd465b
githubmhjao <ntumhjao@gmail.com>
2021-01-16 21:08:55 +0800

    Update routes.py
    handle Notify

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
+# 利用 route 處理路由
 from app import app, handler
+from app.custom_models import AlmaNotify
 
 from flask import request, send_from_directory, abort
 from linebot.exceptions import InvalidSignatureError
@@ -10,6 +12,17 @@ def index():
 @app.route("/result/<token>")
 def get_image_url(token):
     return send_from_directory('/tmp/', filename=f'{token}_DualTone.png')
+
+@app.route("/callback/notify", methods=['GET'])
+def callback_notify():
+    
+    assert request.headers['referer'] == 'https://notify-bot.line.me/'
+    code = request.args.get('code')
+    state = request.args.get('state')
+    
+    success = AlmaNotify.handle_subscribe(code, state)
+
+    return '恭喜完成訂閱！請關閉此視窗。'
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
