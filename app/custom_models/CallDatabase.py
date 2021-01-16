3578f047bb9b2f766bef4a4c27466958fc7cb1f6
githubmhjao <ntumhjao@gmail.com>
2021-01-16 21:16:27 +0800

    Update CallDatabase.py

--- app/custom_models/CallDatabase.py
+++ app/custom_models/CallDatabase.py
@@ -13,15 +13,26 @@ def init_table():
     cursor.execute(postgres_table_query)
     table_records = cursor.fetchall()
     table_records = [i[0] for i in table_records]
+    print('CallDatabase: table_records', table_records)
 
     if 'user_dualtone_settings' not in table_records:
         create_table_query = """CREATE TABLE user_dualtone_settings (
             user_id VARCHAR ( 50 ) PRIMARY KEY,
             message_id VARCHAR ( 50 ) NOT NULL,
-            original_grayscale VARCHAR ( 10 ) NOT NULL,
-            adjusted_grayscale VARCHAR ( 10 ) NOT NULL,
-            first_tone VARCHAR ( 50 ) NOT NULL,
-            second_tone VARCHAR ( 50 ) NOT NULL
+            mode VARCHAR ( 20 ) NOT NULL,
+            gradient_factor VARCHAR ( 20 ) NOT NULL,
+            first_tone VARCHAR ( 20 ) NOT NULL,
+            second_tone VARCHAR ( 20 ) NOT NULL
+        );"""
+
+        cursor.execute(create_table_query)
+        conn.commit()
+
+    if 'notify_subscription' not in table_records:
+        create_table_query = """CREATE TABLE notify_subscription (
+            user_id VARCHAR ( 50 ) PRIMARY KEY,
+            access_token VARCHAR ( 50 ) NOT NULL,
+            subscribe BOOLEAN NOT NULL
         );"""
 
         cursor.execute(create_table_query)
@@ -29,13 +40,14 @@ def init_table():
 
     return True
 
+# Handle Table: user_dualtone_settings
 def init_record(user_id, message_id):
     conn, cursor = access_database()
     
-    table_columns = '(user_id, message_id, original_grayscale, adjusted_grayscale, first_tone, second_tone)'
+    table_columns = '(user_id, message_id, mode, gradient_factor, first_tone, second_tone)'
     postgres_insert_query = f"INSERT INTO user_dualtone_settings {table_columns} VALUES (%s,%s,%s,%s,%s,%s)"
 
-    record = (user_id, message_id, '0', '150', '0:185:0', '0:0:185')
+    record = (user_id, message_id, 'blend', '50', 'red', 'blue')
 
     cursor.execute(postgres_insert_query, record)
     conn.commit()
@@ -71,3 +83,35 @@ def check_record(user_id):
     user_settings = cursor.fetchone()
     
     return user_settings
+
+# Handle Table: notify_subscription
+def notify_get_token(user_id):
+    conn, cursor = access_database()
+    
+    postgres_select_query = f"SELECT * FROM notify_subscription WHERE user_id = '{user_id}';"
+    
+    cursor.execute(postgres_select_query)
+    record = cursor.fetchone()
+    
+    return record
+
+def notify_subscribe(user_id, access_token, subscribe):
+    conn, cursor = access_database()
+
+    if notify_get_token(user_id):
+        postgres_delete_query = "DELETE from notify_subscription where user_id = %s"
+        cursor.execute(postgres_delete_query, (user_id, ))
+        conn.commit()
+
+    table_columns = '(user_id, access_token, subscribe)'
+    postgres_insert_query = f"INSERT INTO notify_subscription {table_columns} VALUES (%s,%s,%s)"
+
+    record = (user_id, access_token, subscribe)
+    
+    cursor.execute(postgres_insert_query, record)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    
+    return record
